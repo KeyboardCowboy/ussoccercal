e14db93ee8779af504c4423b0130100dd458ca61
Chris-Albrecht <Chris.Albrecht@ibm.com>
2019-06-04 22:15:21 -0600

    Remove calendars. Add disclaimer.

--- bin/build.php
+++ bin/build.php
@@ -14,30 +14,30 @@ require_once __DIR__ . '/init.php';
 
 $reports = new ReportBuilder();
 
-if ($cal = Calendars\USSoccerCal::load(CAL_DATA_SOURCE . '/usmnt.yml')) {
-  $cal->generateCalendar();
-  $reports->addReport($cal);
-}
-
-if ($cal = Calendars\USSoccerCal::load(CAL_DATA_SOURCE . '/uswnt.yml')) {
-  $cal->generateCalendar();
-  $reports->addReport($cal);
-}
-
-if ($cal = Calendars\USSoccerCal::load(CAL_DATA_SOURCE . '/ussoccer.yml')) {
-  $cal->generateCalendar();
-  $reports->addReport($cal);
-}
-
-if ($cal = Calendars\USSoccerCal::load(CAL_DATA_SOURCE . '/goldcup-us-2019.yml')) {
-  $cal->generateCalendar();
-  $reports->addReport($cal);
-}
-
-/** @var \CalDom\Calendar\Calendar $cal */
-if ($cal = Calendars\GoldCup2019::load(CAL_DATA_SOURCE . '/goldcup-2019.yml')) {
-  $cal->generateCalendar();
-  $reports->addReport($cal);
-}
+//if ($cal = Calendars\USSoccerCal::load(CAL_DATA_SOURCE . '/usmnt.yml')) {
+//  $cal->generateCalendar();
+//  $reports->addReport($cal);
+//}
+//
+//if ($cal = Calendars\USSoccerCal::load(CAL_DATA_SOURCE . '/uswnt.yml')) {
+//  $cal->generateCalendar();
+//  $reports->addReport($cal);
+//}
+//
+//if ($cal = Calendars\USSoccerCal::load(CAL_DATA_SOURCE . '/ussoccer.yml')) {
+//  $cal->generateCalendar();
+//  $reports->addReport($cal);
+//}
+//
+//if ($cal = Calendars\USSoccerCal::load(CAL_DATA_SOURCE . '/goldcup-us-2019.yml')) {
+//  $cal->generateCalendar();
+//  $reports->addReport($cal);
+//}
+//
+///** @var \CalDom\Calendar\Calendar $cal */
+//if ($cal = Calendars\GoldCup2019::load(CAL_DATA_SOURCE . '/goldcup-2019.yml')) {
+//  $cal->generateCalendar();
+//  $reports->addReport($cal);
+//}
 
 $reports->writeReport();
